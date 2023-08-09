38acea5c540ce42f82134abe0c89f59a64ce9f6d
Grigory Shipunov <blame@oxapentane.com>
2023-08-09 22:48:22 +0200

    cleanup a bit

--- shittynnet/tensor.py
+++ shittynnet/tensor.py
@@ -45,54 +45,54 @@ class Tensor:
   def __add__(self, other):
     other = other if isinstance(other, Tensor) else Tensor(other)
     out = Tensor(np.add(self.data, other.data), (self, other), '+')
-
     def _backward():
-      self.grad += out._val_shape(out.grad)
-      other.grad += out._val_shape(out.grad)
+      self.grad += out.grad
+      other.grad += out.grad
     out._backward = _backward
-
     return out
 
   def __mul__(self, other):
     other = other if isinstance(other, Tensor) else Tensor(other)
+    assert self._shape == other._shape
     out = Tensor(self.data * other.data, (self, other), '*')
-
     def _backward():
-      self.grad += self._val_shape(out.grad) * other.data
-      other.grad += self._val_shape(out.grad) * self.data
+      self.grad += out.grad * other.data
+      other.grad += out.grad * self.data
     out._backward = _backward
-
     return out
 
   # element-wise unary ops
   def __pow__(self, exponent):
     assert isinstance(exponent, (float, int)), "power implemented only for float and int exponents"
     out = Tensor(self.data ** exponent, (self,), f'**{exponent}')
-
     def _backward():
       self.grad += (exponent * self.data**(exponent - 1)) * out.grad
     out._backward = _backward
-
     return out
   
   def exp(self):
-    out = Tensor(np.exp(self.data), (self,), 'exp')
+    out = Tensor(np.nan_to_num(np.exp(self.data)), (self,), 'exp')
     def _backward():
-      self.grad += np.exp(self.data) * out.grad
+      self.grad += np.nan_to_num(np.exp(self.data)) * out.grad
     out._backward = _backward
     return out
-    
+
+  def mean(self):
+    out = Tensor(self.data.mean(), (self,), 'mean')
+    def _backward():
+      self.grad += self._val_shape(out.grad) / self.data.size
+    out._backward = _backward
+    return out
+
   # Activation functions
   def relu(self):
     out = Tensor(self.data * (self.data > 0), (self,), 'ReLU')
-
     def _backward():
       self.grad += out.grad * (out.data > 0)
     out._backward = _backward
-
     return out
 
-  def sigm(self): return 1 / (1 + (-self).exp())
+  def sigm(self): return ((-self).exp() + self._val_shape(1))**-1
   def softmax(self): raise NotImplementedError
 
   # generation helpers
@@ -104,13 +104,15 @@ class Tensor:
   def __sub__(self, other): return  self + (-other)
   def __rsub__(self, other): return  (-self) + other
   def __radd__(self, other): return self + other
-  def __neg__(self): return self * -1
+  def __neg__(self): return self * self._val_shape(-1)
   def __truediv__(self, other): return self * other**-1
   def __rtruediv__(self, other): return other * self**-1
+
   # internal grad helpers
   def _zero_grad(self): self.grad = np.zeros(self._shape)
   def _one_grad(self): self.grad =  np.ones(self._shape)
   def _val_shape(self, val): return np.ones(self._shape) * val
+
   def _build_topo(self):
     topo = []
     visited = set()
